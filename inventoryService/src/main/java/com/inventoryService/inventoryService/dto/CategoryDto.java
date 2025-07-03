2095d379757eef18333772eaf807582ab6fb1bc6
SohilPalasara <sohilpalasara10@gmail.com>
2025-07-03 11:58:57 +0530

    category Dto add

---
+++ inventoryService/src/main/java/com/inventoryService/inventoryService/dto/CategoryDto.java
@@ -0,0 +1,54 @@
+package com.inventoryService.inventoryService.dto;
+
+import com.inventoryService.inventoryService.entity.Category;
+import com.inventoryService.inventoryService.entity.Organization;
+import com.inventoryService.inventoryService.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryDto {
+    private String id;
+    private String categoryName;
+    private String description;
+    private  boolean isDeleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Status status;
+    private String parentCategoryId;
+    private String organizationId;
+
+    public Category convertToEntity( Category parentCategory, Organization organization) {
+        return Category.builder()
+                .categoryName(this.categoryName)
+                .description(this.description)
+                .isDeleted(this.isDeleted())
+                .status(Status.ACTIVE)
+                .parentCategory(parentCategory)
+                .organization(organization)
+                .build();
+    }
+    public static  CategoryDto convertToDto(Category category){
+       return  CategoryDto.builder()
+               .id(category.getId())
+               .categoryName(category.getCategoryName())
+               .description(category.getDescription())
+               .isDeleted(category.isDeleted())
+               .status(category.getStatus())
+               .createdAt(category.getCreatedAt())
+               .updatedAt(category.getUpdatedAt())
+               .organizationId(category.getOrganization().getOrganizationId())
+               .parentCategoryId(category.getParentCategory().getId())
+
+                .build();
+    }
+
+
+}
