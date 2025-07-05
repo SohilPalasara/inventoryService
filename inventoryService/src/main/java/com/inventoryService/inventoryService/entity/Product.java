c130c0a8c83b98388e7b13471f45a5faa146eb59
SohilPalasara <sohilpalasara10@gmail.com>
2025-07-05 12:44:04 +0530

    product entity add

---
+++ inventoryService/src/main/java/com/inventoryService/inventoryService/entity/Product.java
@@ -0,0 +1,40 @@
+package com.inventoryService.inventoryService.entity;
+
+import com.inventoryService.inventoryService.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    private String productId;
+    private String productName;
+    private String description;
+    private String productCode;
+    private String productImage;
+
+    private  boolean isDeleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @ManyToOne
+    private Category category;
+
+    @PrePersist
+    private void setValue(){
+        this.productId= UUID.randomUUID().toString();
+        this.createdAt=LocalDateTime.now();
+        this.updatedAt=LocalDateTime.now();
+    }
+}
